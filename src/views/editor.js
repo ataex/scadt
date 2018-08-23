a8280d14dc2ae325ef7942165f88d0c90622a5f5
omudrejshij <omudrejshij@gmail.com>
2018-08-23 23:57:42 +0300

    Add status bar and corret engine to core

--- src/views/editor.js
+++ src/views/editor.js
@@ -1,13 +1,17 @@
 import m from 'mithril';
 import LeftPanel from '../components/left-panel.js';
+import StatusBar from '../components/status-bar.js';
 
 export default class Editor {
-	view() {
-		return m('.editor.view', [m(LeftPanel)]);
+	view(vnode) {
+		return m('.editor.view', [
+			m(LeftPanel, { core: vnode.attrs.core }),
+			m(StatusBar, { core: vnode.attrs.core }),
+		]);
 	}
 
 	oncreate(vnode) {
-		const engine = vnode.attrs.engine;
+		const engine = vnode.attrs.core.engine;
 		vnode.dom.appendChild(engine.viewport);
 		engine.updateViewportSize();
 	}
