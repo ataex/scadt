52ef28abb9f256b4e88986685f3f29824ffab89d
omudrejshij <omudrejshij@gmail.com>
2018-08-23 23:56:48 +0300

    Fix main app structure and initialization

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import './styles/main.less';
 import m from 'mithril';
 
-import Engine from './engine/engine.js';
 import Core from './core/core';
 import EditorView from './views/editor.js';
 import AboutView from './views/about.js';
 
-const engine = new Engine();
-const core = new Core(engine);
+const core = new Core();
 
 m.route(document.body, '/', {
-	'/': { view: () => m(EditorView, { engine: engine }) },
+	'/': { view: () => m(EditorView, { core }) },
 	'/about': AboutView,
 });
