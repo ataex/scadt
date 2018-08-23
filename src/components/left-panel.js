b102a78302001418593315ab53897deaf6d3b948
omudrejshij <omudrejshij@gmail.com>
2018-08-24 00:01:37 +0300

    Add onclick function to buttons

--- src/components/left-panel.js
+++ src/components/left-panel.js
@@ -2,7 +2,7 @@ import m from 'mithril';
 import Button from './button.js';
 
 export default class LeftPanel {
-	view() {
+	view(vnode) {
 		return m('.left.panel', [
 			m(Button, {
 				icon: 'folder',
@@ -10,7 +10,25 @@ export default class LeftPanel {
 					document.querySelector('#file-upload').click();
 				},
 			}),
-			m(Button, { icon: 'home' }),
+			m(Button, { icon: 'fullscreen' }),
+			m(Button, {
+				icon: 'move',
+				onclick() {
+					vnode.attrs.core.mode('move');
+				},
+			}),
+			m(Button, {
+				icon: 'rotate',
+				onclick() {
+					vnode.attrs.core.mode('rotate');
+				},
+			}),
+			m(Button, {
+				icon: 'scale',
+				onclick() {
+					vnode.attrs.core.mode('scale');
+				},
+			}),
 			m('input', { id: 'file-upload', type: 'file', hidden: true }),
 		]);
 	}
